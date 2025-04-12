4de16ee905d574e4d144cf115243c20434f00b03
Pedrozo0901 <felipepedrozoribeiro@hotmail.com>
2025-04-12 10:22:51 -0400

    feat: crud produtos

--- DeuOrca/DeuOrca/settings.py
+++ DeuOrca/DeuOrca/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'orcamentos',
+    'produtos', 
     'pdfs',
     'rest_framework',  # se for usar API com Django REST
     'corsheaders',      # se tiver front separado
@@ -79,12 +80,8 @@ WSGI_APPLICATION = 'DeuOrca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DeuOrca',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
